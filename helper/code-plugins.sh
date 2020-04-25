83527e9b139a383ae978d383571234c0b7e715df
Felix Hungenberg <30648730+shiftgeist@users.noreply.github.com>
2020-04-25 16:38:35 +0200

    Add helper code plugins and pacman base scripts

---
+++ helper/code-plugins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+code --install-extension jolaleye.horizon-theme-vscode
+code --install-extension pkief.material-icon-theme
+code --install-extension editorconfig.editorconfig
+code --install-extension coenraads.bracket-pair-colorizer
+code --install-extension ow.vscode-subword-navigation
+code --install-extension wakatime.vscode-wakatime

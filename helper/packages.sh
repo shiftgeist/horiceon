cc8f2da4555bca284f51bcca563388776ce4d610
Felix Hungenberg <30648730+shiftgeist@users.noreply.github.com>
2020-05-06 17:38:51 +0200

    Remove nvm add nodejs

--- helper/packages.sh
+++ helper/packages.sh
@@ -2,4 +2,4 @@
 
 # script to install the much needed packages
 
-yay -Syu i3-gaps-rounded-git vim zsh openssh ttf-dejavu noto-fonts-emoji kitty rofi dunst htop feh code neofetch maim xdg-utils pulseaudio compton
+yay -Syu i3-gaps-rounded-git vim zsh openssh ttf-dejavu noto-fonts-emoji kitty rofi dunst htop feh code neofetch maim xdg-utils pulseaudio compton nodejs

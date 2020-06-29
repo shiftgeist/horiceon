d995297c5fc5595b1f14f22a2c2a90c569b4ab8d
Felix Hungenberg <30648730+shiftgeist@users.noreply.github.com>
2020-06-29 11:07:55 +0200

    update vscode plugin bracket-pair-colorizer to version 2

--- helper/code-plugins.sh
+++ helper/code-plugins.sh
@@ -2,6 +2,6 @@
 
 code --install-extension pkief.material-icon-theme
 code --install-extension editorconfig.editorconfig
-code --install-extension coenraads.bracket-pair-colorizer
+code --install-extension coenraads.bracket-pair-colorizer-2
 code --install-extension ow.vscode-subword-navigation
 code --install-extension wakatime.vscode-wakatime

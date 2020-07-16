9eb9d0f833040eab018ee007b0fa236768ccc47b
shiftgeist <git@shiftgeist.com>
2020-07-16 14:01:14 +0200

    enhance code-plugins.sh

--- helper/code-plugins.sh
+++ helper/code-plugins.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-code --install-extension pkief.material-icon-theme
-code --install-extension editorconfig.editorconfig
-code --install-extension coenraads.bracket-pair-colorizer-2
-code --install-extension ow.vscode-subword-navigation
-code --install-extension wakatime.vscode-wakatime
+PLUGINS=(
+    coenraads.bracket-pair-colorizer-2
+    editorconfig.editorconfig
+    ow.vscode-subword-navigation
+    wakatime.vscode-wakatime
+)
+
+OPTIONAL=(
+    bierner.markdown-preview-github-styles
+    naumovs.color-highlight
+    pkief.material-icon-theme
+    usernamehw.errorlens
+)
+
+echo "Install optional plugins? [Yn]"
+read ans
+case $ans in
+    Y  ) echo "yes"; PLUGINS+=(${OPTIONAL[@]}); break;;
+    N  ) echo "no"; break;;
+    "" ) echo "yes"; PLUGINS+=(${OPTIONAL[@]}); break;;
+esac
+
+for i in ${PLUGINS[@]}
+do
+    code --install-extension $i
+done

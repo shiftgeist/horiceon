d95c4b6bbbd5e41fa3a05b7a4e7a63a37fcd7539
shiftgeist <git@shiftgeist.com>
2020-07-23 09:59:47 +0200

    cleanup repo

--- helper/packages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# script to install the much needed packages
-
-yay -Syu i3-gaps-rounded-git vim zsh openssh rsync ttf-dejavu noto-fonts-emoji kitty rofi dunst htop feh code neofetch maim xdg-utils pulseaudio compton nodejs npm go
---
+++ helper/yay-core.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# script to install the much needed packages
+
+yay -Syu i3-gaps vim zsh openssh rsync ttf-dejavu noto-fonts-emoji otf-overpass kitty rofi dunst htop feh code neofetch maim xdg-utils pulseaudio compton nodejs npm flameshot

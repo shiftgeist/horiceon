71454b3798f552136857e2b70ec1012110c5423a
Felix Hungenberg <30648730+shiftgeist@users.noreply.github.com>
2020-08-03 14:06:07 +0200

    cleanup bin folder

--- helper/yay-core.sh
+++ helper/yay-core.sh
@@ -2,4 +2,4 @@
 
 # script to install the much needed packages
 
-yay -Syu i3-gaps vim zsh openssh rsync ttf-dejavu noto-fonts-emoji otf-overpass kitty rofi dunst htop feh code neofetch maim xdg-utils pulseaudio compton nodejs npm flameshot
+yay -Syu i3-gaps vim zsh openssh rsync ttf-dejavu noto-fonts-emoji otf-overpass kitty tmux st rofi dunst htop feh code neofetch maim xdg-utils pulseaudio compton nodejs npm flameshot

8937926b2fa343ae61fe4f786f3493a2ccfb9565
Felix Hungenberg <30648730+shiftgeist@users.noreply.github.com>
2020-08-06 13:54:27 +0200

    update "what's included"

--- dot_rice/executable_yay-core.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-yay -Syu i3-gaps vim zsh openssh rsync ttf-dejavu noto-fonts-emoji otf-overpass kitty tmux st rofi dunst htop feh code neofetch maim xdg-utils pulseaudio compton nodejs npm flameshot

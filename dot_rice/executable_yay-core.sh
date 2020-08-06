81125a1a3feddbd86c01f07b943c3910758abc34
Felix Hungenberg <30648730+shiftgeist@users.noreply.github.com>
2020-08-06 13:47:41 +0200

    add .rice/helper, add tinypng config

---
+++ dot_rice/executable_yay-core.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yay -Syu i3-gaps vim zsh openssh rsync ttf-dejavu noto-fonts-emoji otf-overpass kitty tmux st rofi dunst htop feh code neofetch maim xdg-utils pulseaudio compton nodejs npm flameshot

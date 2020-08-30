433a426f0886d2915b3a90021be9b36838de4167
shiftgeist <git@shiftgeist.com>
2020-08-30 17:31:07 +0200

    add markdown lint to global packages

--- dot_local/share/horiceon/executable_npm-globals.sh
+++ dot_local/share/horiceon/executable_npm-globals.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-npm i -g npm-check-updates sirv-cli live-server doctoc @bitwarden/cli tinypng-cli firebase-tools
+npm i -g npm-check-updates sirv-cli live-server doctoc @bitwarden/cli tinypng-cli firebase-tools markdownlint-cli

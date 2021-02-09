23ed3e2987f817df4c0efc0178ca04b17f113b7a
Felix Hungenberg <30648730+shiftgeist@users.noreply.github.com>
2021-02-09 16:33:20 +0100

    make checkout of master branch dynamic to main branch

--- dot_config/zsh/git.helper.zsh
+++ dot_config/zsh/git.helper.zsh
@@ -19,6 +19,19 @@ git_is_clean() {
   fi
 }
 
+# Check if main exists and use instead of master
+git_main_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  local branch
+  for branch in main trunk; do
+    if command git show-ref -q --verify refs/heads/$branch; then
+      echo $branch
+      return
+    fi
+  done
+  echo master
+}
+
 # Outputs the name of the current branch
 __git_current_branch() {
   git_is_repo || return
